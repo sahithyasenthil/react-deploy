b45f5c5475613c7f26082765f78bbe7e6f5873f7
SS086401 <Sahithya.Senthilkumar@cerner.com>
2021-08-23 12:51:01 +0530

    Deploying in gh pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import profileimage from'./img/pic.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React App</h1>
+      <img src={profileimage} alt="profile-image" height='100px' width='100px'/>
     </div>
   );
 }
